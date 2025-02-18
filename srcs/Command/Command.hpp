fb8ef424532b4a971553be355ae941b5350c296e
Gabriel Rivo <garivo@z3r5p6.42lyon.fr>
2025-02-18 18:03:47 +0100

    Nick remake, weird behavior with join AGAIN

--- srcs/Command/Command.hpp
+++ srcs/Command/Command.hpp
@@ -20,7 +20,7 @@ class Command
 		static void CatchErrors(Client *client, const std::exception& e);
 			//Command List
 		static void JoinChannel(Client client, Message message, Server &server);
-		static void Nick(Message message, Client &sender, Server server);
+		static void Nick(Message message, Client &sender, Server &server);
 		static void Part(Message message, Client &sender, Server &server);
 		static void PrivateMessage(Message message, Client sender, Server server);
 		static void QuitClient(int fd, Poll &server, size_t i);
--- srcs/Command/Nick.cpp
+++ srcs/Command/Nick.cpp
@@ -1,6 +1,6 @@
 #include "Command.hpp"
 
-void Command::Nick(Message message, Client &sender, Server server)
+void Command::Nick(Message message, Client &sender, Server &server)
 {
 	int fdcl;
 
@@ -12,40 +12,41 @@ void Command::Nick(Message message, Client &sender, Server server)
 		std::cout << ":" << sender.GetNick() << "!" << sender.GetName() << "@" << sender.GetIpAdd() << " NICK " << message.getContent() << std::endl;
 		fdcl = sender.GetFd();
 		Command::WritePrefix(fdcl, sender);
-		sender.SetNick(message.getContent());
 		std::string response = " NICK " + message.getContent() + "\n";
 		send(fdcl, response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
-		return;
 	}
-
-	for (;it != server.getChannel().end(); it++)
+	else
 	{
-		std::vector<std::string> sentclient;
-		std::map<int, Client>::iterator itcl = (it->second).GetClient().begin();
-		for (;itcl != (it->second).GetClient().end(); itcl++)
+		for (;it != server.getChannel().end(); it++)
 		{
-			if (itcl->second.GetName() == sender.GetName())
+			std::vector<std::string> sentclient;
+			std::map<int, Client>::iterator itcl = (it->second).GetClient().begin();
+			for (;itcl != (it->second).GetClient().end(); itcl++)
 			{
-				std::map<int, Client>::iterator itcl2 = (it->second).GetClient().begin();
-				for (;itcl2 != (it->second).GetClient().end(); itcl2++)
+				if (itcl->second.GetNick() == sender.GetNick())
 				{
-					std::vector<std::string>::iterator vit = std::find(sentclient.begin(), sentclient.end(), itcl2->second.GetName());
-					if (vit == sentclient.end())
+					itcl->second.SetNick(message.getContent());
+					std::map<int, Client>::iterator itcl2 = (it->second).GetClient().begin();
+					for (;itcl2 != (it->second).GetClient().end(); itcl2++)
 					{
-						sentclient.push_back(itcl2->second.GetName());
-						//printf("Le nick la hein|%p|\n", &(itcl2->second));
-						std::cout << "We sent nick to " << itcl2->second.GetName() << std::endl;
-						std::cout << ":" << sender.GetNick() << "!" << sender.GetName() << "@" << sender.GetIpAdd() << " NICK " << message.getContent() << std::endl;
-						fdcl = (itcl2->second).GetFd();
-						std::cout << fdcl << std::endl;
-						Command::WritePrefix(fdcl, sender);
-						sender.SetNick(message.getContent());
-						std::string response = " NICK " + message.getContent() + "\n";
-						send(fdcl, response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
+						std::vector<std::string>::iterator vit = std::find(sentclient.begin(), sentclient.end(), itcl2->second.GetNick());
+						if (vit == sentclient.end())
+						{
+							sentclient.push_back(itcl2->second.GetNick());
+							//printf("Le nick la hein|%p|\n", &(itcl2->second));
+							std::cout << "We sent nick to " << itcl2->second.GetNick() << std::endl;
+							std::cout << ":" << sender.GetNick() << "!" << sender.GetName() << "@" << sender.GetIpAdd() << " NICK " << message.getContent() << std::endl;
+							fdcl = (itcl2->second).GetFd();
+							std::cout << fdcl << std::endl;
+							Command::WritePrefix(fdcl, sender);
+							std::string response = " NICK " + message.getContent() + "\n";
+							send(fdcl, response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
+						}
 					}
+					break;
 				}
-				break;
 			}
 		}
 	}
+	sender.SetNick(message.getContent());
 }
