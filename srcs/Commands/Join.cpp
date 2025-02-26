747231e51450b0059d3fcb9f38cc84c1749e31c9
Samuel Perrier <saperrie@z2r12p3.42lyon.fr>
2025-02-26 17:35:38 +0100

    add : 	Kick prints who was kicked by whom to all clients in channel
    next :
    	print Reason for kick (need to parse it first I guess and add variable to message.hpp)
    	Handle multiple kick
    	Add additionnal error cases

--- srcs/Commands/Join.cpp
+++ srcs/Commands/Join.cpp
@@ -6,7 +6,7 @@
 /*   By: saperrie <saperrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 12:14:14 by saperrie          #+#    #+#             */
-/*   Updated: 2025/02/25 15:19:48 by saperrie         ###   ########.fr       */
+/*   Updated: 2025/02/26 14:35:38 by saperrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,24 @@ void Command::JoinChannel(Client &client, Message message, Server &server)
 	if (message.getTo().find('#') != 0 && message.getTo().find('&') != 0)
 		throw ProtocolError(ERR_NOSUCHCHANNEL, message.getTo(), client.GetNick());
 	std::map<std::string, Channel>::iterator it = server.getChannel().find(message.getTo());
-    if (server.getChannel().empty() == false &&  it != server.getChannel().end())
+	if (server.getChannel().empty() == false && it != server.getChannel().end())
 	{
-       it->second.JoinChannel(&client);
-	   std::cout << client.GetName() << " join " << message.getTo() << std::endl;
-    }
+		it->second.JoinChannel(&client);
+		std::cout << client.GetName() << " join " << message.getTo() << std::endl;
+	}
 	else
 	{
 		std::cout << "creating " << message.getTo() << std::endl;
 		server.getChannel()[message.getTo()] = server.CreateChannel(&client, message.getTo());
-    }
+	}
 	write_channel(client, message, server);
 }
 
 static void write_channel(Client client, Message message, Server &server)
 {
-	//:Nick!User@Host JOIN #general
-	std::map<int, Client*>::iterator	itclient = server.getClients().begin();
-	std::string							response;
+	//: Nick!User@Host JOIN #general
+	std::map<int, Client *>::iterator itclient = server.getClients().begin();
+	std::string response;
 
 	while (itclient != server.getClients().end())
 	{
--- srcs/Commands/Kick.cpp
+++ srcs/Commands/Kick.cpp
@@ -6,33 +6,59 @@
 /*   By: saperrie <saperrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 12:14:17 by saperrie          #+#    #+#             */
-/*   Updated: 2025/02/25 18:18:19 by saperrie         ###   ########.fr       */
+/*   Updated: 2025/02/26 17:15:00 by saperrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Command.hpp"
 #include "Channel.hpp"
 
-// check si l'user est l'ope
-// check si l'utilisateur d'en face existe
-// s'aider de la commade part
+
+// SEND MESSAGE TO HEXCHAT AND ALL CLIENTS ON CHANNEL
+// HANDLE MULTIPLE CLIENTS TO KICK
+// INSERT REASON FOR KICK
 
 void Command::Kick(Message message, Client &client, Server &server)
 {
+	int client_fd;
+	std::string response;
 	std::cout << "KICK cmd :" << std::endl;
+
 	std::map<std::string, Channel>::iterator channel_it = server.getChannel().find(message.getTo());
 	Channel channel = channel_it->second;
 	if (channel_it == server.getChannel().end())
-		throw ProtocolError(ERR_NOSUCHCHANNEL, message.getTo(), client.GetNick());
-	std::map<int, Client*>::iterator toBeKicked_it = channel.GetClient().begin();
+	throw ProtocolError(ERR_NOSUCHCHANNEL, message.getTo(), client.GetNick());
 
-	for (; toBeKicked_it != channel.GetClient().end(); toBeKicked_it++)
-		if ((*toBeKicked_it->second).GetNick() == message.getContent())
+	int	isOperator = channel_it->second.IsOperator(client.GetFd());
+	if (isOperator == false)
+	throw ProtocolError(ERR_CHANOPRIVSNEEDED, message.getTo(), client.GetNick());
+
+	std::string clientOpWantsToKick = message.getContent();
+	std::map<int, Client*>::iterator client_it = channel.GetClient().begin();
+	for (; client_it != channel.GetClient().end(); client_it++)
+	{
+		if ((*client_it->second).GetNick() == clientOpWantsToKick)
+		{
+			// std::cout << ":" << client.GetNick() << "!" << client.GetName() << "@" << client.GetIpAdd() << " PART " << message.getTo() << " " << message.getContent() << std::endl;
+			// client_fd = (*client_it->second).GetFd();
+			// std::cout << client_fd << std::endl;
+			// response = GetPrefix(client);
+			// response += " KICK " + message.getTo() + " " + message.getContent() + "\r\n";
+			// send(client_fd, response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
+			SendBySharedChannels(response, client, server);
+			channel.PartChannel((*client_it->second));
 			break;
-	if (toBeKicked_it == channel.GetClient().end())
-		throw ProtocolError(ERR_USERNOTINCHANNEL, message.getContent() + " " + channel.getName(),  client.GetNick());
-	if (channel_it->second.IsOperator(client.GetFd()) == false)
-		throw ProtocolError(ERR_CHANOPRIVSNEEDED, message.getTo(), client.GetNick());
+		}
+	}
+	if (client_it == channel.GetClient().end())
+		throw ProtocolError(ERR_USERNOTINCHANNEL, clientOpWantsToKick + " " + channel.getName(),  client.GetNick());
 
-	channel.PartChannel((*toBeKicked_it->second));
+	client_it = channel.GetClient().begin();
+	for (; client_it != channel.GetClient().end(); ++client_it)
+	{
+		client_fd = (*client_it->second).GetFd();
+		response = GetPrefix(client);
+		response += " KICK " + message.getTo() + " " + message.getContent() + "\r\n";
+		send(client_fd, response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
+	}
 }
