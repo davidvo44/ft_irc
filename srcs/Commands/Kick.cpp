b3b20ac67b735b7d3b950110b839c91c00ec770d
Samuel Perrier <saperrie@z2r12p3.42lyon.fr>
2025-02-28 15:13:10 +0100

    edit : 	kick command getting closer to the goal
    next :
    	add reason for kick (parse it) and add default reason if none provided by op

--- srcs/Commands/Kick.cpp
+++ srcs/Commands/Kick.cpp
@@ -3,27 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   Kick.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvo <dvo@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: saperrie <saperrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 12:14:17 by saperrie          #+#    #+#             */
-/*   Updated: 2025/02/28 08:56:19 by dvo              ###   ########.fr       */
+/*   Updated: 2025/02/28 15:12:34 by saperrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Command.hpp"
 #include "Channel.hpp"
 
-
-// SEND MESSAGE TO HEXCHAT AND ALL CLIENTS ON CHANNEL
-// HANDLE MULTIPLE CLIENTS TO KICK
-// INSERT REASON FOR KICK
 /*
 	std::map<std::string, Channel*>::iterator channel_it = server.getChannel().find(message.getTo());
 	Channel *channel = channel_it->second;
 	peut etre remplace par:
 	Channel *channel = server.getChannel().findValue(message.getTo());
 
-	
+
 	if (channel_it == server.getChannel().end())
 	peut etre remplace par:
 	if (!channel)
@@ -38,48 +34,54 @@
 	{
 	idx++
 	}
-	
+
 */
-void Command::Kick(Message message, Client &client, Server &server)
+
+// INSERT REASON FOR KICK AND INSERT KICK MESSAGE (+ CREATE DEFAULT KICK MESSAGE IF NONE PROVIDED)
+
+static void	notifyAllClientsUserWasKicked(Client &client, std::map<int, Client*>::iterator client_it, Channel *channel, Server &server, std::string response);
+
+void Command::Kick(Message message, Client &opClient, Server &server)
 {
-	int client_fd;
-	std::string response;
+	std::string	response;
 	std::cout << "KICK cmd :" << std::endl;
-	server.getChannel().findValue(message.getTo());
+
 	std::map<std::string, Channel*>::iterator channel_it = server.getChannel().find(message.getTo());
 	Channel *channel = channel_it->second;
 	if (channel_it == server.getChannel().end())
-		throw ProtocolError(ERR_NOSUCHCHANNEL, message.getTo(), client.GetNick());
+		throw ProtocolError(ERR_NOSUCHCHANNEL, message.getTo(), opClient.GetNick());
 
-	int	isOperator = channel_it->second->IsOperator(client.GetFd());
+	int	isOperator = channel_it->second->IsOperator(opClient.GetFd());
 	if (isOperator == false)
-		throw ProtocolError(ERR_CHANOPRIVSNEEDED, message.getTo(), client.GetNick());
-	std::string clientOpWantsToKick = message.getContent();
+		throw ProtocolError(ERR_CHANOPRIVSNEEDED, message.getTo(), opClient.GetNick());
+
+	if (message.getContent().empty())
+		throw ProtocolError(ERR_NEEDMOREPARAMS, message.getTo(), opClient.GetNick());
+	std::string userToKickNick = message.getContent();
+
 	std::map<int, Client*>::iterator client_it = channel->GetClient().begin();
 	for (; client_it != channel->GetClient().end(); client_it++)
 	{
-		if ((*client_it->second).GetNick() == clientOpWantsToKick)
+		if ((*client_it->second).GetNick() == userToKickNick)
 		{
-			// std::cout << ":" << client.GetNick() << "!" << client.GetName() << "@" << client.GetIpAdd() << " PART " << message.getTo() << " " << message.getContent() << std::endl;
-			// client_fd = (*client_it->second).GetFd();
-			// std::cout << client_fd << std::endl;
-			// response = GetPrefix(client);
-			// response += " KICK " + message.getTo() + " " + message.getContent() + "\r\n";
-			// send(client_fd, response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
-			SendBySharedChannels(response, client, server);
+			response = opClient.GetPrefix();
+			response += " KICK " + message.getTo() + " " + message.getContent() + "\r\n";
+			SendBySharedChannels(response, opClient, server);
 			channel->PartChannel((*client_it->second));
 			break;
 		}
 	}
 	if (client_it == channel->GetClient().end())
-		throw ProtocolError(ERR_USERNOTINCHANNEL, clientOpWantsToKick + " " + channel->getName(),  client.GetNick());
+		throw ProtocolError(ERR_USERNOTINCHANNEL, message.getTo(), userToKickNick);
 
-	client_it = channel->GetClient().begin();
+	notifyAllClientsUserWasKicked(opClient, channel->GetClient().begin(), channel, server, response);
+}
+
+void	notifyAllClientsUserWasKicked(Client &client, std::map<int, Client*>::iterator client_it, Channel *channel, Server &server, std::string response)
+{
 	for (; client_it != channel->GetClient().end(); ++client_it)
 	{
-		client_fd = (*client_it->second).GetFd();
-		response = client.GetPrefix();
-		response += " KICK " + message.getTo() + " " + message.getContent() + "\r\n";
-		send(client_fd, response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
+		Command::SendBySharedChannels(response, client, server);
+		send((*client_it->second).GetFd(), response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
 	}
 }
