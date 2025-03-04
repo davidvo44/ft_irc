1ef63890ee4dccfd3f6775c67ac3ded01a519bfb
David.V <david.vo@laposte.net>
2025-03-04 23:13:15 +0100

    add all condition

--- srcs/RplMessage.cpp
+++ srcs/RplMessage.cpp
@@ -6,7 +6,7 @@
 /*   By: dvo <dvo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 12:14:58 by saperrie          #+#    #+#             */
-/*   Updated: 2025/03/03 19:13:54 by dvo              ###   ########.fr       */
+/*   Updated: 2025/03/04 21:37:33 by dvo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,12 @@ void RplMessage::GetRply(int code, int fd, int size, const char * value, ...)
 			break;
 		case RPL_WHOREPLY:
 			final_msg = arg_list[1];
+		case RPL_NAMREPLY:
+			final_msg = "= " + arg_list[1] + " :" + arg_list[2];
+			break;
+		case RPL_ENDOFNAMES:
+			final_msg = "= " + arg_list[1] + " ::End of /NAMES list";
+			break;
 	}
 	std::string response = ":irc.com " + scode + " " + arg_list[0] + " " + final_msg + "\r\n";
     send(fd, response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
