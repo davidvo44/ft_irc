aa89db6d91359d8d3d938666ff6349d35000b6ed
Samuel Perrier <saperrie@z3r12p2.42lyon.fr>
2025-03-10 02:18:37 +0100

    refactor : 	couple tweaks to KICK using MutantMap 	made smaller functions within to look for target to kick and send kick message to other clients on channel

--- srcs/Commands/Kick.cpp
+++ srcs/Commands/Kick.cpp
@@ -3,29 +3,25 @@
 #include "Channel.hpp"
 #include <sstream>
 
+static void searchTargetClient(Message &message, Channel &channel, Client &source);
+static void sendKickMessageToAllClientsOnChannel(Channel &channel, std::string response);
+
 void Command::Kick(Message& message, Client &source, Server &server)
 {
-	std::string	response;
-	std::string	reasonForKick = "Inappropriate behaviour";
 	std::cout << "KICK cmd :" << std::endl;
 
 	message.parseKICK();
 	std::string targetNick = message.getParameter();
 
-	std::cout << message.getCommand() << " | " << message.getTarget() << " | "  << message.getParameter() << " | " << message.getSuffix();
 	if (source.getLogStep() != 3)
 		throw ProtocolError(ERR_NOTREGISTERED, source.getNick(), source.getNick());
 
-	std::map<std::string, Channel*>::iterator channel_it = server.getChannel().find(message.getTarget());
-
-	if (channel_it == server.getChannel().end())
+	Channel *channel = server.getChannel().findValue(message.getTarget());
+	if (!channel)
 		throw ProtocolError(ERR_NOSUCHCHANNEL, message.getTarget(), source.getNick());
-	Channel *channel = channel_it->second;
 
-	std::map<int, Client*>::iterator source_it = channel->getClient().begin();
-	for (; source_it != channel->getClient().end(); source_it++)
-	{}
-	if (source_it == channel->getClient().end())
+	Client	*sourceClient = channel->getClient().findValue(source.getFd());
+	if (!sourceClient)
 		throw ProtocolError(ERR_NOTONCHANNEL, message.getTarget(), source.getNick());
 
 	if (channel->isOperator(source.getFd()) == false)
@@ -34,25 +30,38 @@ void Command::Kick(Message& message, Client &source, Server &server)
 	if (targetNick.empty() == true)
 		throw ProtocolError(ERR_NEEDMOREPARAMS, message.getCommand(), source.getNick());
 
-	std::map<int, Client*>::iterator client_it = channel->getClient().begin();
-	for (; client_it != channel->getClient().end(); client_it++)
+	searchTargetClient(message, *channel, source);
+}
+
+void searchTargetClient(Message &message, Channel &channel, Client &source)
+{
+	std::string	response;
+	std::string	reasonForKick = "Inappropriate behaviour";
+	std::string targetNick = message.getParameter();
+	unsigned int i = 0;
+
+	while (channel[i])
 	{
-		if ((*client_it->second).getNick() == targetNick)
-		{
-			if (!message.getSuffix().empty())
-				reasonForKick = message.getSuffix();
-			response = source.getPrefix();
-			response += " KICK " + message.getTarget() + " " + targetNick + " " + reasonForKick + "\r\n";
-			send((*client_it->second).getFd(), response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
-			channel->partChannel((*client_it->second));
+		if (channel[i]->getNick() == targetNick)
 			break;
-		}
+		i++;
 	}
+
 	std::string sourceNickPlusClientNick = source.getNick() + " " + targetNick;
-	if (client_it == channel->getClient().end())
+	if (!channel[i])
 		throw ProtocolError(ERR_USERNOTINCHANNEL, message.getTarget(), sourceNickPlusClientNick);
 
-	client_it = channel->getClient().begin();
-	for (; client_it != channel->getClient().end(); ++client_it)
-		send((*client_it->second).getFd(), response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
+	reasonForKick = message.getSuffix();
+	response = source.getPrefix();
+	response += " KICK " + message.getTarget() + " " + targetNick + " " + reasonForKick + "\r\n";
+	send(channel[i]->getFd(), response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
+	channel.partChannel(*channel[i]);
+
+	sendKickMessageToAllClientsOnChannel(channel, response);
+}
+
+void sendKickMessageToAllClientsOnChannel(Channel &channel, std::string response)
+{
+	for (unsigned int i = 0; channel[i]; i++)
+		send(channel[i]->getFd(), response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
 }
