bbcb663162d0a59bf3c53286c97ad557eb9fa578
David.V <david.vo@laposte.net>
2025-03-13 01:04:06 +0100

    add mode +t

--- srcs/Commands/Topic.cpp
+++ srcs/Commands/Topic.cpp
@@ -1,6 +1,8 @@
 
 #include "Command.hpp"
 
+static void setTopic(Client &sender, Channel &chan, Message &message);
+
 void Command::Topic(Message& message, Client &sender, Server &server)
 {
 	message.parsePRIVMSG_PART_TOPIC();
@@ -17,7 +19,7 @@ void Command::Topic(Message& message, Client &sender, Server &server)
 	if (message.getParameter().empty() == true)
 		getTopic(message , sender, *chan);
 	else
-		chan->setTopic(message.getParameter());
+		setTopic(sender, *chan, message);
 }
 
 void Command::getTopic(Message& message, Client &sender, Channel &chan)
@@ -29,4 +31,14 @@ void Command::getTopic(Message& message, Client &sender, Channel &chan)
 	else
 		response = RPL_TOPIC(sender.getNick(), message.getTarget(), chan.getTopic());
 	send(sender.getFd(), response.c_str(), response.length(), MSG_DONTWAIT | MSG_NOSIGNAL);
+}
+
+static void setTopic(Client &sender, Channel &chan, Message &message)
+{
+	if (chan.viewMode('t') == true)
+	{
+		if (chan.isOperator(sender.getFd()) == false)
+			throw ProtocolError(ERR_CHANOPRIVSNEEDED, message.getTarget(), sender.getNick());
+	}
+	chan.setTopic(message.getParameter());
 }
