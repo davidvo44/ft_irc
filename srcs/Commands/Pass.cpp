9194abcea5fdc2fcb6c611318a2b21e526cef0ca
David.V <dvo@student.42lyon.fr>
2025-03-18 19:48:36 +0100

    repair if pass is empty

--- srcs/Commands/Pass.cpp
+++ srcs/Commands/Pass.cpp
@@ -5,11 +5,11 @@ void Command::PassCommand(Server &server, Client &sender, Message &message)
 {
 	message.parseNICK_USER_PASS();
 
-	if (message.getParameter().empty() == true)
+	if (message.getParameter().empty() == true && server.getPassword().empty() == false)
 		throw ProtocolError(ERR_NEEDMOREPARAMS, message.getTarget(), sender.getNick());
 	if (sender.getLogStep() != 0)
 		throw ProtocolError(ERR_ALREADYREGISTERED, sender.getNick(), sender.getNick());
-	if (message.getParameter() != server.getPassword())
+	if (server.getPassword().empty() == false && message.getParameter() != server.getPassword())
 		throw ProtocolError(ERR_PASSWDMISMATCH, sender.getNick(), sender.getNick());
 	sender.setLogStep(1);
 }
